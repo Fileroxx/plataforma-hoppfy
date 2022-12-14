bfb621e4c2590a021788e780e4b36c1e30dd2793
Igor <igor_cesari@hotmail.com>
2022-12-14 12:52:36 -0300

    js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 export default {
-  root: path.resolve(__dirname, 'src/dist'),
+  root: path.resolve(__dirname, 'src/dist/'),
   server: {
     port: 8080,
     hot: true
